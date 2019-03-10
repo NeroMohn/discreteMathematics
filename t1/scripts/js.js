551132621abaeae16411db031e4fcb5330e0a75d
David Rodrigues <davidrod.contato@gmail.com>
2019-03-10 17:52:16 -0300

    All inputs working

--- t1/scripts/js.js
+++ t1/scripts/js.js
@@ -2,48 +2,77 @@
 let fs = require = ("fs");
 let textFromFileLoadedGlobal;
 let groups = [];
+let grouped = [];
 let elements = [];
 let values = [];
 
-function txtInput(){
-let file = document.getElementById("file").files[0];
+function nullFilter(value) {
+	return value != null;
+}
 
-	  const fileReader = new FileReader();
+function txtInput() {
+	let file = document.getElementById("file").files[0];
 
-	  fileReader.onload = function(fileLoadedEvent){
-		  let textFromFileLoaded = fileLoadedEvent.target.result; //The event.result property contains the last/previous value returned by an event handler triggered by the specified event. From w3schools
+	const fileReader = new FileReader();
 
-		  textFromFileLoadedGlobal = textFromFileLoaded;
+	fileReader.onload = function (fileLoadedEvent) {
+		let textFromFileLoaded = fileLoadedEvent.target.result; //The event.result property contains the last/previous value returned by an event handler triggered by the specified event. From w3schools
 
-		  document.getElementById("inputTextToSave").value = textFromFileLoadedGlobal;
-	  }
+		textFromFileLoadedGlobal = textFromFileLoaded;
 
-	  fileReader.readAsText(file, "UTF-8");
+		document.getElementById("inputTextToSave").value = textFromFileLoadedGlobal;
+	}
 
-    }
+	fileReader.readAsText(file, "UTF-8");
 
-	function setGroups(){
+}
 
-		let textForUse;
-		
-		textForUse = textFromFileLoadedGlobal;
-		textForUse = textForUse.replace(/ /g,"");
-		textForUse = textForUse.replace(/FIM/g,"");
-		console.log(textForUse);
-		let reg = /\n/;
-		let regGroup = /[a-zA-Z]/;	
-		textForUse = textForUse.split(reg);	
-		textForUse.pop();
-		console.log(textForUse);
+function setGroups() {
 
-		for(i=0; i<textForUse.length;i++){
-			groups.push(textForUse[i].match(regGroup));
-		}
-		for(i=0; i<textForUse.length;i++){
-			groups.push(textForUse[i].match(regGroup));
-		}
-		console.log(groups)
+	let textForUse;
+
+	textForUse = textFromFileLoadedGlobal;
+	textForUse = textForUse.replace(/ /g, "");
+	textForUse = textForUse.replace(/FIM/g, "");
+	console.log(textForUse);
+	let reg = /\n/;
+	let regGrouped = /[a-zA-Z]/;
+	let regElement = /[a-z]/;
+	let regGroup = /[A-Z]/;
+	textForUse = textForUse.split(reg);
+	textForUse.pop();
+	console.log(textForUse);
+	for (i = 0; i < textForUse.length; i++) {
+		grouped.push(textForUse[i].match(regGrouped));
+	}
+
+	for (j = 0; j < grouped.length; j++) {
+		elements.push(grouped[j].input.match(regElement));
+	}
+	elements = elements.filter(nullFilter);
+
+	for (k = 0; k < grouped.length; k++) {
+		groups.push(grouped[k].input.match(regGroup));
+	}
+	groups = groups.filter(nullFilter);
+
+	for (k = 0; k < grouped.length; k++) {
+		values.push(grouped[k].input);
+		values[k] = values[k].replace(/[a-zA-Z]/g, "");
+		values[k] = values[k].replace(/\=/g, "");
+		values[k] = values[k].replace(/\{/g, "");
+		values[k] = values[k].replace(/\}/g, "");
+	}
+
+	console.log(grouped)
+	console.log(groups)
+	console.log(elements);
+	console.log(values);
 
 
 	document.getElementById("inputGroups").value = groups;
+	document.getElementById("inputElements").value = elements;
+	for (i = 0; i < grouped.length; i++) {
+		document.getElementById("inputValues").value += grouped[i][0] + " = " + values[i];
 	}
+}
