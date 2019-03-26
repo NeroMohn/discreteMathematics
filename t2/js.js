d343a8fb873e36672d9548a1f33d699b9996940a
David Rodrigues <davidrod.contato@gmail.com>
2019-03-26 01:57:15 -0300

    Não terminado a classificação

--- t2/js.js
+++ t2/js.js
@@ -63,6 +63,7 @@ function maiorQue(){
         }
     }
 
+ /*   let controle=0;
     for(i=0;i<groupB.length;i++){
         for(j=0;j<resultado.length;j++){
             if(groupB[i]!=resultado[j][1]){
@@ -71,25 +72,29 @@ function maiorQue(){
         }
     }
 
-    for(i=0;i<groupA.length;i++){
+    for(i=0;i<groupB.length;i++){
         for(j=0;j<resultado.length;j++){
-            if(groupB[i]!=resultado[j][0]){
-                injetora = 0;
+            if(groupB[i]==resultado[j][1]){
+                controle++;
             }
+            if(controle>1)
+                injetora=0;
+            controle=0;
         }
     }
 
 
-    for(i=0;i<resultado.length;i++){
-    document.getElementById("maiorQue").value +="("+resultado[i]+")";
-    }
+
     if(injetora == 1)
         document.getElementById("maiorQueClassificacao").value +="|Injetora| ";
     if(sobrejetora == 1)
         document.getElementById("maiorQueClassificacao").value +="|Sobrejetora| ";
     if(sobrejetora == 1 && injetora ==1)
         document.getElementById("maiorQueClassificacao").value +="|Total| ";
-
+*/
+        for(i=0;i<resultado.length;i++){
+            document.getElementById("maiorQue").value +="("+resultado[i]+")";
+            }
 return resultado;
 }
 
@@ -110,9 +115,41 @@ function menorQue(){
             }
         }
     }
+    /*let controle=0;
+    for(i=0;i<groupB.length;i++){
+        for(j=0;j<resultado.length;j++){
+            if(groupB[i]!=resultado[j][1]){
+                sobrejetora = 0;
+            }
+        }
+    }
+
+    for(i=0;i<groupB.length;i++){
+        for(j=0;j<resultado.length;j++){
+            if(groupB[i]==resultado[j][1]){
+                controle++;
+            }
+            if(controle>1)
+                injetora=0;
+            controle=0;
+        }
+    }
+
+
+
+    if(injetora == 1)
+        document.getElementById("menorQueClassificacao").value +="|Injetora| ";
+    if(sobrejetora == 1)
+        document.getElementById("menorQueClassificacao").value +="|Sobrejetora| ";
+    if(sobrejetora == 1 && injetora ==1)
+        document.getElementById("menorQueClassificacao").value +="|Total| ";
+
+*/
     for(i=0;i<resultado.length;i++){
     document.getElementById("menorQue").value +="("+resultado[i]+")";
     }
+
+    
 return resultado;
 }
 
@@ -133,6 +170,36 @@ function igualA(){
             }
         }
     }
+    /*let controle=0;
+    for(i=0;i<groupB.length;i++){
+        for(j=0;j<resultado.length;j++){
+            if(groupB[i]!=resultado[j][1]){
+                sobrejetora = 0;
+            }
+        }
+    }
+
+    for(i=0;i<groupB.length;i++){
+        for(j=0;j<resultado.length;j++){
+            if(groupB[i]==resultado[j][1]){
+                controle++;
+            }
+            if(controle>1)
+                injetora=0;
+            controle=0;
+        }
+    }
+
+
+
+    if(injetora == 1)
+        document.getElementById("igualAClassificacao").value +="|Injetora| ";
+    if(sobrejetora == 1)
+        document.getElementById("igualAClassificacao").value +="|Sobrejetora| ";
+    if(sobrejetora == 1 && injetora ==1)
+        document.getElementById("igualAClassificacao").value +="|Total| ";
+
+*/
     for(i=0;i<resultado.length;i++){
     document.getElementById("igualA").value +="("+resultado[i]+")";
     }
@@ -156,6 +223,36 @@ function quadradoDe(){
             }
         }
     }
+    /*let controle=0;
+    for(i=0;i<groupB.length;i++){
+        for(j=0;j<resultado.length;j++){
+            if(groupB[i]!=resultado[j][1]){
+                sobrejetora = 0;
+            }
+        }
+    }
+
+    for(i=0;i<groupB.length;i++){
+        for(j=0;j<resultado.length;j++){
+            if(groupB[i]==resultado[j][1]){
+                controle++;
+            }
+            if(controle>1)
+                injetora=0;
+            controle=0;
+        }
+    }
+
+
+
+    if(injetora == 1)
+        document.getElementById("quadradoDeClassificacao").value +="|Injetora| ";
+    if(sobrejetora == 1)
+        document.getElementById("quadradoDeClassificacao").value +="|Sobrejetora| ";
+    if(sobrejetora == 1 && injetora ==1)
+        document.getElementById("quadradoDeClassificacao").value +="|Total| ";
+
+*/
     for(i=0;i<resultado.length;i++){
     document.getElementById("quadradoDe").value +="("+resultado[i]+")";
     }
@@ -179,6 +276,35 @@ function raizQuadradaDe(){
             }
         }
     }
+   /* let controle=0;
+    for(i=0;i<groupB.length;i++){
+        for(j=0;j<resultado.length;j++){
+            if(groupB[i]!=resultado[j][1]){
+                sobrejetora = 0;
+            }
+        }
+    }
+
+    for(i=0;i<groupB.length;i++){
+        for(j=0;j<resultado.length;j++){
+            if(groupB[i]==resultado[j][1]){
+                controle++;
+            }
+            if(controle>1)
+                injetora=0;
+            controle=0;
+        }
+    }
+
+
+
+    if(injetora == 1)
+        document.getElementById("raizQuadradaDeClassificacao").value +="|Injetora| ";
+    if(sobrejetora == 1)
+        document.getElementById("raizQuadradaDeClassificacao").value +="|Sobrejetora| ";
+    if(sobrejetora == 1 && injetora ==1)
+        document.getElementById("raizQuadradaDeClassificacao").value +="|Total| ";
+*/
     for(i=0;i<resultado.length;i++){
     document.getElementById("raizQuadradoDe").value +="("+resultado[i]+")";
     }
